3314cbfd1f0bbe759b930eaee9cf9ab4a200082c
Hani CHALOUATI <hani.chalouati@gmail.com>
2026-02-08 15:38:24 +0100

    remove cli

--- apps/cli/src/infrastructure/workspace/cli-workspace-mode.service.ts
+++
@@ -1,8 +0,0 @@
-import { WorkspaceRuntimeEnum } from '@qwery/domain/enums';
-import { WorkspaceRuntimeService } from '@qwery/domain/services';
-
-export class CliWorkspaceModeService extends WorkspaceRuntimeService {
-  public async detectWorkspaceRuntime(): Promise<WorkspaceRuntimeEnum> {
-    return WorkspaceRuntimeEnum.DESKTOP;
-  }
-}
